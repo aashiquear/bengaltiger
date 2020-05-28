759acf5778b5010be708af21773ad5feb2adab87
Aashique Rezwan <aashiquear@gmail.com>
2020-05-28 15:35:11 -0500

    Correction

--- src/kernels/RecombinationRate.C
+++ src/kernels/RecombinationRate.C
@@ -19,7 +19,7 @@ template <ComputeStage compute_stage>
 InputParameters
 RecombinationRate<compute_stage>::validParams()
 {
-  InputParameters params = Kernel::validParams();
+  InputParameters params = ADKernelValue<compute_stage>::validParams();
   params.addClassDescription("Recombination rate for the radiation induced segregation");
   params.addParam<Real>("value", 1.0, "Coefficient to multiply by the body force term");
   params.addParam<MaterialPropertyName>("K", "K_iv", "Property name for rate coefficient");
@@ -49,6 +49,6 @@ template <ComputeStage compute_stage>
 ADReal
 RecombinationRate<compute_stage>::precomputeQpResidual()
 {
-  ADReal rate = _K[_qp] * _omega[_qp] * _u[_qp] * _v[_qp];
+  Real rate = _K[_qp] * _omega[_qp] * _u[_qp] * _v[_qp];
   return -_scale * _postprocessor * _function.value(_t, _q_point[_qp]) * rate;
 }
